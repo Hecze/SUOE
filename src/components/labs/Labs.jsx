a60d55f8084e690661feff6348064e0b65a8985d
Hecze <hectorzerrillo85@gmail.com>
2023-11-02 16:33:48 -0500

    mi second commit

--- src/components/labs/Labs.jsx
+++ src/components/labs/Labs.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./Labs.css";
+import Image from "next/image";
 
 function Labs() {
   const [antiguoPabellon, setAntiguoPabellon] = useState([]);
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar () {
     <nav className="navbar">
       <ul className="nav-list">
         <div className="logo" id="fisi">
-        <Image src="/fisi.png" alt="" />
+        <Image src="/fisi.png" width={"300"} height={"300"} alt="" />
         <h2>ESPACIOS FISI</h2>
         </div>
         <li className="nav-item"><Link href="/">Inicio</Link></li>
@@ -17,7 +17,7 @@ function Navbar () {
         <li className="nav-item"><Link href="/LabsPage">Laboratorios</Link></li>
         <li className="nav-item"><Link href="https://mrsquatch.github.io/Reserva-de-Espacio-FISI/">Otros espacios</Link></li>
         <div className="logo" id="unmsm">
-        <Image src="/unmsm.png" alt="" />
+        <Image src="/unmsm.png" width={"150"} height={"40"} alt="" />
         </div>
 
       </ul>
