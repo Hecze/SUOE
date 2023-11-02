e63969f144993d95497f0914913c5d76232ffa0a
Hecze <hectorzerrillo85@gmail.com>
2023-11-02 16:38:18 -0500

    mi thrid commit

---
+++ src/components/lab/Lab.jsx
@@ -0,0 +1,15 @@
+import "./Lab.css"
+
+
+function Lab () {
+  return (
+    <>
+    <div className="">
+        <h1>Lab</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam</p>
+    </div>
+    </>
+  );
+}
+
+export default Lab;
